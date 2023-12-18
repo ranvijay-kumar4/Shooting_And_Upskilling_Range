54a9548bf78903d84169a94038e446a49f9134b5
RANVIJAY KUMAR <ranvijaykumarupadhyay903@gmail.com>
2023-12-18 15:32:17 +0530

    Folder readded succesfully

---
+++ C++/Code_Help/vector.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    // ----------------------------------------------------
+    // vector<int> array(5,0);
+    // for (int i = 0; i < array.size(); i++)
+    // {
+    //     cout << array[i] << " " ;
+    // }
+
+    // ----------------------------------------------------
+    // vector<int> array{10,20,30,40,50} ;
+    // for (int i = 0; i < array.size(); i++)
+    // {
+    //     cout << array[i] << " " ;
+    // }
+
+    // ----------------------------------------------------
+    // int n ;
+    // cin >> n ;
+    // vector<int> array(n);
+    // for (int i = 0; i < n; i++)
+    // {
+    //     cin >> array[i] ;
+    // }
+    // for (int j = 0; j < array.size(); j++)
+    // {
+    //     cout << array[j] << " ";
+    // }
+
+    // ----------------------------------------------------
+    // vector<int> array(5);
+    // array.push_back(1);
+    // array.push_back(2);
+    // array.push_back(3);
+    // array.push_back(4);
+    // array.push_back(5);
+    // cout << array.size() << endl;
+    // cout << array.capacity() << endl;
+    // cout << array.empty() << endl;
+    // for (int i = 0; i < array.size(); i++)
+    // {
+    //     cout << array[i] << " ";
+    // }
+    // array.pop_back();
+    // cout << endl << array.size() << endl;
+    //     for (int i = 0; i < array.size(); i++)
+    // {
+    //     cout << array[i] << " " ;
+    // }
+
+    // ----------------------------------------------------
+    // Finding Unique Element from Array
+    // ----------------------------------------------------
+    // Union Of two Arrays
+    // ----------------------------------------------------
+    // Intersection of Two Arrays
+    // ----------------------------------------------------
+    // Pair SUM // Two SUM
+    // ----------------------------------------------------
+    // Triplet Sum // Three SUM
+    // ----------------------------------------------------
+    // Four SUM
+    // ----------------------------------------------------
+    // Sort 0's and 1's in Array
+    // ----------------------------------------------------
+    // Left Rotate an array by one element
+    // ----------------------------------------------------
+    // Majority element in array
+    // ----------------------------------------------------
+    // Buy and Sell Stock
+    // ----------------------------------------------------
+
+    // ----------------------------------------------------
+
+    return 0;
+}
