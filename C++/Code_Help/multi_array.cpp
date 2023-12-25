a3075f003c82ca63b6bad94ac344eee48597db2e
RANVIJAY KUMAR <ranvijaykumarupadhyay903@gmail.com>
2023-12-26 04:40:28 +0530

    Various Assignment Solved

--- C++/Code_Help/multi_array.cpp
+++ C++/Code_Help/multi_array.cpp
@@ -137,6 +137,115 @@ int main()
     //     cout << endl;
     // }
 
+    // -------------------------------------Method 1
+    // Sort 0's, 1's & 2's
+    // ----------------------------------------------------
+    // // Test Case : a = 2, 0, 2, 1, 1, 0 : Output = 0, 0, 1, 1, 2, 2;
+    // int arr[] = {2, 0, 2, 1, 1, 0}, temp;
+    // for (int j = 0; j < 6; j++)
+    // {
+    //     for (int i = 0; i < 6; i++)
+    //     {
+    //         if (arr[i] > arr[i + 1])
+    //         {
+    //             temp = arr[i];
+    //             arr[i] = arr[i + 1];
+    //             arr[i + 1] = temp;
+    //         }
+    //     }
+    // }
+
+    // for (int i = 0; i < 6; i++)
+    // {
+    //     cout << arr[i] << " ";
+    // }
+
+    // -------------------------------------Method 2
+    // vector<int> arr{2, 0, 2, 1, 1, 0};
+    // int left = 0, i = 0, right = arr.size() - 1;
+    // while (i <= right)
+    // {
+    //     if (arr[i] == 0)
+    //     {
+    //         swap(arr[i], arr[left]);
+    //         left++;
+    //         i++;
+    //     }
+    //     else if (arr[i] == 2)
+    //     {
+    //         swap(arr[i], arr[right]);
+    //         right--;
+    //     }
+    //     else
+    //     {
+    //         i++;
+    //     }
+    // }
+
+    // for (int i = 0; i < arr.size(); i++)
+    // {
+    //     cout << arr[i] << " ";
+    // }
+
+    // Move all -ve no to one side of array
+    // ----------------------------------------------------
+    // vector<int> arr{1, 2, -3, 4, -5, 6};
+    // int left = 0, right = arr.size() - 1;
+    // for (int i = 0; i < arr.size(); i++)
+    // {
+    //     if (arr[i] < 0)
+    //     {
+    //         swap(arr[i], arr[left]);
+    //         left++;
+    //     }
+    // }
+    // for (int i = 0; i < arr.size(); i++)
+    // {
+    //     cout << arr[i] << " ";
+    // }
+
+    // Find Duplicate Element
+    // ----------------------------------------------------
+    // -------------------------Time Complexity is very high--
+    // vector<int> arr{1, 2, 3, 4, 6, 3};
+    // for (int i = 0; i < arr.size(); i++)
+    // {
+    //     for (int j = i + 1; j < arr.size(); j++)
+    //     {
+    //         if (arr[i] == arr[j])
+    //         {
+    //             cout << arr[i] << " ";
+    //             break;
+    //         }
+    //     }
+    // }
+
+    // Find mising Element
+    // ----------------------------------------------------
+
+    // Find first repeating element
+    // ----------------------------------------------------
+    // --------------------------High Complexity-
+    // vector<int> arr{10, 5, 3, 4, 3, 5, 6};
+    // for (int i = 0; i < arr.size(); i++)
+    // {
+    //     for (int j = i+1; j < arr.size(); j++)
+    //     {
+    //         if (arr[i] == arr[j])
+    //     {
+    //         cout << arr[i];
+    //         break;
+    //     }
+    //     }        
+    // }
+    
+
+    // Find common element in 3 arrays
+    // ----------------------------------------------------
+
+    // Factorial of Large number
+    // ----------------------------------------------------
+
     return 0;
 }
 
