77783efa695c70bab0aadf13aa4c328d5fff4b38
RANVIJAY KUMAR <ranvijaykumarupadhyay903@gmail.com>
2023-12-26 04:42:41 +0530

    Unknown file

--- C++/Code_Help/tempCodeRunnerFile.cpp
+++ C++/Code_Help/tempCodeRunnerFile.cpp
@@ -1,2 +1,12 @@
-start++ ;
-   end-- ;
+ctor<int> arr{1, 2, 3, 4, 6, 3};
+    for (int i = 0; i < arr.size(); i++)
+    {
+        for (int j = i + 1; j < arr.size(); j++)
+        {
+            if (arr[i] == arr[j])
+            {
+                cout << arr[i] << " ";
+                break;
+            }
+        }
+    }
