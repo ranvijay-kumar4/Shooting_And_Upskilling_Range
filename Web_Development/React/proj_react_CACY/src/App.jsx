430a4ae2bd66fba73fbb16cf19df0ec2900d0a7a
RANVIJAY KUMAR <ranvijaykumarupadhyay903@gmail.com>
2024-03-25 03:20:22 +0530

    Props practised

--- Web_Development/React/proj_react_CACY/src/App.jsx
+++ Web_Development/React/proj_react_CACY/src/App.jsx
@@ -18,6 +18,40 @@ function App() {
       setCounter(counter--);
     }
   }
+
+  /* 
+   const addvalue = () => {
+      if (counter <= 20) {
+        setCounter(counter++);
+        setCounter(counter++);
+        setCounter(counter++);
+        setCounter(counter++);
+        setCounter(counter++);
+        
+      }
+    }
+    the above code will return as usual that 1 click increases 1 no at a time
+
+    first result will be 11
+
+    Since useState combines in a batch using React fibre and works single time that's where different method will be used
+
+    that is setCounter provides a callback function as a hidden feature
+    const addvalue = () => {
+      if (counter <= 20) {
+        setCounter((counter) => counter++ );
+        setCounter((counter) => counter++);
+        setCounter((counter) => counter++);
+        setCounter((counter) => counter++);
+        setCounter((counter) => counter++);
+        
+        now the output will me 15 directly by one click
+      }
+    }
+
+  */
+
+
   return (
     <>
       <h1 className='bg'>Hello World</h1>
@@ -32,7 +66,7 @@ function App() {
 
       <h1 className='bg-slate-100 text-black rounded-xl m-4'>Ranvijay</h1>
 
-      <Card />
+      <Card username = "Ranvijay" btnText = "visit us" />
       <br />
       <Card />
     </>
--- Web_Development/React/proj_react_CACY/src/Components/Card.jsx
+++ Web_Development/React/proj_react_CACY/src/Components/Card.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
-
-function Card() {
+// username is a prop written i place of prop.username
+// on adding props we have to handleit too
+function Card({username, btnText= "visit me"}) {
     return (
         <div className="w-[300px] rounded-md border">
             <img
@@ -9,7 +10,7 @@ function Card() {
                 className="h-[200px] w-full rounded-md object-cover"
             />
             <div className="p-4">
-                <h1 className="text-lg font-semibold">About Macbook</h1>
+                <h1 className="text-lg font-semibold">{username}</h1>
                 <p className="mt-3 text-sm text-gray-600">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
                     debitis?
@@ -18,7 +19,8 @@ function Card() {
                     type="button"
                     className="mt-4 rounded-sm bg-black px-2.5 py-1 text-[10px] font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
                 >
-                    Read
+                    {btnText}
+                    {/* {btnText || "Visit me"}  default value if not provided further, its alternate code can be directly written on top i.e. btnText = " " */}
                 </button>
             </div>
         </div>
