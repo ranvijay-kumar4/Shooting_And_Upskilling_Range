17acc6736962de23a4e4fde3c47f47861fddca08
RANVIJAY KUMAR <ranvijaykumarupadhyay903@gmail.com>
2024-05-31 09:59:33 +0530

    understanding selection sort

---
+++ C++/Code_Help/sorting.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    vector<int> arr{5, 4, 3, 2, 1};
+    int n = arr.size();
+    for (int i = 0; i < n - 1; i++)
+    {
+        int min_index = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[min_index])
+            {
+                min_index = j;
+            }
+        }
+        swap(arr[i], arr[min_index]);
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
+
