36cabf044f0122f47b9bbc23de93c71b343e982b
RANVIJAY KUMAR <ranvijaykumarupadhyay903@gmail.com>
2024-06-13 12:09:57 +0530

    Backend development file and Rotating hover effect added

---
+++ Web_Development/Backend/First/server.js
@@ -0,0 +1,17 @@
+// console.log("Hello World");
+
+// Need of including the below line to access the prompt function
+// from NPM rompt sync 
+// var prompt = require('prompt-sync')();
+// ans = prompt("Hello! This is a Prompt, Need a value : ");
+// console.log(ans);
+
+
+
+// -----------Callback Function--------------
+
+function add(a, b) {
+    return a + b;
+}
+result = add(2, 3);
+console.log(result);
