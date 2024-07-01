8bca0fac91218c1f50b8829918a54c13f19fab5f
RANVIJAY KUMAR <ranvijaykumarupadhyay903@gmail.com>
2024-07-01 19:16:42 +0530

    BG Changer

--- Web_Development/React/Bg_Changer/src/App.jsx
+++ Web_Development/React/Bg_Changer/src/App.jsx
@@ -1,16 +1,33 @@
-import { useState } from "react"
-
+import React, { useState } from 'react'
 
 function App() {
 
-  const [color, setColor] = useState("black")
+  const [color, setColor] = useState("black");
+  const [counter, setCounter] = useState(0);
+
+
   return (
+
     <div className="w-full h-screen duration-200"
       // Can be also written as : style={{backgroundColor: "#000"}}
-
       style={{ backgroundColor: color }}>
+      <div className="my-auto flex justify-center flex-col items-center">
+        <h1 className='text-5xl m-2 font-bold border-4 border-solid rounded-lg bg-white'>Count : {counter}</h1>
+
+        <button className='text-xl border-4 border-solid p-1 gap-2 m-1 rounded-lg bg-green-500 color text-white' onClick={() => {
+          setCounter(counter + 1);
+        }}>Increment</button>
+
+        <button className='text-xl border-4 border-solid p-1 gap-2 m-1 rounded-lg bg-red-500 color text-white' onClick={() => {
+          if (counter > 0) {
+            setCounter(counter - 1);
+          }
+        }}>Decrement</button>
+      </div>
       <div className=" fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+
         <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-2 py-1 rounded-3xl">
+
           <button
             onClick={() => setColor("Red")}
             className="outline-none px-4 py-2 rounded-full text-white shadow-lg " style={{ backgroundColor: "Red" }}>
